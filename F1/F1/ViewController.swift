671d67b21d03330ab3c313abe19695cc5bbdfc94
Kaitlyn Sookdhew <89077850+Kaityss@users.noreply.github.com>
2024-04-02 13:27:35 +0200

    Project Setup
    Initial Setup of the project

---
+++ F1/F1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  F1
+//
+//  Created by Kaitlyn Sookdhew on 2024/04/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
