05c1490a60ac6492053d23cc7c3b35f589ce7455
Ksookdhew <89077850+Ksookdhew@users.noreply.github.com>
2024-05-09 10:26:05 +0200

    Clean up

--- F1/F1/Utilities/ViewController+Extension.swift
+++ F1/F1/Utilities/ViewController+Extension.swift
@@ -5,10 +5,10 @@
 //  Created by Kaitlyn Sookdhew on 2024/05/09.
 //
 
-import Foundation
 import UIKit
 
-extension UIViewController{
+extension UIViewController {
+    
     func showAlert(alertTitle: String, alertMessage: String) {
         let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
