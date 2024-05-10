662b5af36b25a4e508dbb56f0b7b2ba1ce406f88
Ksookdhew <89077850+Ksookdhew@users.noreply.github.com>
2024-05-10 08:29:37 +0200

    Clean up

--- F1/F1/Race/Repository/RaceRepository.swift
+++ F1/F1/Race/Repository/RaceRepository.swift
@@ -12,13 +12,13 @@ typealias RaceResults = (Result< Racing, APIError>) -> Void
 
 // MARK: Protocol
 protocol RaceRepositoryType: AnyObject {
-    func fetchRaceResults(completion: @escaping(RaceResults))
+    func fetchRaceResults(completion: @escaping RaceResults)
 }
 
 // MARK: Repository
 class RaceRepository: RaceRepositoryType {
 
-    func fetchRaceResults(completion: @escaping (RaceResults)) {
+    func fetchRaceResults(completion: @escaping RaceResults) {
         let url = Endpoints.racing
         URLSession.shared.request(endpoint: url, method: .GET, completion: completion)
     }
