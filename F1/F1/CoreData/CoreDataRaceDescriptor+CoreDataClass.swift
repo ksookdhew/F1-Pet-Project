f276a3ea2c5f67ba2421cf53644b561a584b0e0b
Ksookdhew <89077850+Ksookdhew@users.noreply.github.com>
2024-05-16 08:33:16 +0200

    folder clean up

--- F1/F1/CoreData/CoreDataRaceDescriptor+CoreDataClass.swift
+++ F1/F1/CoreData/CoreDataRaceDescriptor+CoreDataClass.swift
@@ -9,7 +9,12 @@
 import Foundation
 import CoreData
 
-
 public class CoreDataRaceDescriptor: NSManagedObject {
 
+    @NSManaged public var series: String?
+    @NSManaged public var url: String?
+    @NSManaged public var limit: String?
+    @NSManaged public var offset: String?
+    @NSManaged public var total: String?
+    @NSManaged public var raceTable: CoreDataRaceTable?
 }
