26ca90fb293924c4a3868a0736e72a30ae8badbe
Ksookdhew <89077850+Ksookdhew@users.noreply.github.com>
2024-05-26 14:33:05 +0200

    Driver info table view fix

--- F1/F1/Config/AppDelegate.swift
+++ F1/F1/Config/AppDelegate.swift
@@ -13,6 +13,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
          UINavigationBar.appearance().tintColor = .red
+        if #available(iOS 15, *) {
+            // MARK: Navigation bar appearance
+            let navigationBarAppearance = UINavigationBarAppearance()
+            navigationBarAppearance.configureWithOpaqueBackground()
+            navigationBarAppearance.titleTextAttributes = [
+                NSAttributedString.Key.foregroundColor: UIColor.white
+            ]
+            navigationBarAppearance.backgroundColor = .clear
+            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+            UINavigationBar.appearance().compactAppearance = navigationBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+        }
          return true
     }
 
--- F1/F1/Driver/DriverViewController.swift
+++ F1/F1/Driver/DriverViewController.swift
@@ -11,20 +11,7 @@ class DriverViewController: LoadingIndicatorViewController {
 
     // MARK: IBOutlets
     @IBOutlet weak private var tableView: UITableView!
-    @IBOutlet weak private var driverImage: UIImageView!
-    @IBOutlet weak private var driverName: UILabel!
-    @IBOutlet weak private var driverNationality: UILabel!
-    @IBOutlet weak private var driverConstructor: UILabel!
-    @IBOutlet weak private var driverSurname: UILabel!
-    @IBOutlet weak private var currentWins: UILabel!
-    @IBOutlet weak private var currentPoints: UILabel!
-    @IBOutlet weak private var currentPosition: UILabel!
-    @IBOutlet weak private var tableHeading: UILabel!
-    @IBOutlet weak private var winsHeading: UILabel!
-    @IBOutlet weak private var pointsHeading: UILabel!
-    @IBOutlet weak private var posHeading: UILabel!
-    @IBOutlet weak private var statsHeading: UILabel!
-
+    
     // MARK: Variables
     var driver: DriverStanding?
     private lazy var viewModel = DriverViewModel(repository: DriverRepository(), delegate: self)
@@ -42,63 +29,62 @@ class DriverViewController: LoadingIndicatorViewController {
         tableView.dataSource = self
         tableView.register(DriverTableViewCell.nib(),
                            forCellReuseIdentifier: Identifiers.driverTableViewCell)
+        tableView.register(DriverInfoTableViewCell.nib(),
+                           forCellReuseIdentifier: Identifiers.driverInfoTableViewCell)
         tableView.register(DriverResultHeader.nib(),
                            forHeaderFooterViewReuseIdentifier: Identifiers.driverResultsIdentifier)
     }
 
     private func setupView() {
         viewModel.setDriver(driver: driver)
-        driverImage.image = UIImage(named: viewModel.driverImageName)
-        driverName.text = viewModel.driverGivenName
-        driverSurname.text = viewModel.driverFamilyName
-        driverConstructor.text = viewModel.driverConstructorInfo
-        driverNationality.text = viewModel.driverNationality
-        currentPosition.text = viewModel.currentPosition
-        currentPoints.text = viewModel.currentPoints
-        currentWins.text = viewModel.currentWins
-        viewIsHidden(isHidden: true)
+        tableView.isHidden = true
     }
 
-    private func viewIsHidden(isHidden: Bool) {
-        driverImage.isHidden = isHidden
-        driverName.isHidden = isHidden
-        driverSurname.isHidden = isHidden
-        driverConstructor.isHidden = isHidden
-        driverNationality.isHidden = isHidden
-        currentPosition.isHidden = isHidden
-        currentPoints.isHidden = isHidden
-        currentWins.isHidden = isHidden
-        tableView.isHidden = isHidden
-        statsHeading.isHidden = isHidden
-        posHeading.isHidden = isHidden
-        pointsHeading.isHidden = isHidden
-        winsHeading.isHidden = isHidden
-        tableHeading.isHidden = isHidden
-    }
 }
 
 // MARK: - UITableViewDelegate and UITableViewDataSource
 extension DriverViewController: UITableViewDelegate, UITableViewDataSource {
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        viewModel.resultsCount
+        let numberOfRows = [1, viewModel.resultsCount]
+        return numberOfRows[section]
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        42.0
+        let heights = [375.0, 42.0]
+        return heights[indexPath.section]
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView
-            .dequeueReusableCell(withIdentifier: Identifiers.driverTableViewCell) as? DriverTableViewCell else {
-            return UITableViewCell()
+        switch indexPath.section {
+        case 0:
+            guard let cell = tableView
+                .dequeueReusableCell(withIdentifier: Identifiers.driverInfoTableViewCell) as? DriverInfoTableViewCell else {
+                return UITableViewCell()
+            }
+            guard let result = driver else {
+                return UITableViewCell()
+            }
+            cell.populateWith(driver: result)
+            return cell
+
+        default:
+            guard let cell = tableView
+                .dequeueReusableCell(withIdentifier: Identifiers.driverTableViewCell) as? DriverTableViewCell else {
+                return UITableViewCell()
+            }
+            guard let result = viewModel.result(atIndex: indexPath.item) else {
+                return UITableViewCell()
+            }
+            let laptime = viewModel.laptime(index: indexPath.item)
+            cell.populateWith(result: result, lapTime: laptime)
+            return cell
+
         }
-        guard let result = viewModel.result(atIndex: indexPath.item) else {
-            return UITableViewCell()
-        }
-        let laptime = viewModel.laptime(index: indexPath.item)
-        cell.populateWith(result: result, lapTime: laptime)
-        return cell
     }
 
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
@@ -110,7 +96,8 @@ extension DriverViewController: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        45
+        let headerHeights = [0.0, 45.0]
+        return headerHeights[section]
     }
 }
 
@@ -119,7 +106,7 @@ extension  DriverViewController: ViewModelDelegate {
     func reloadView() {
         tableView.reloadData()
         hideLoadingIndicator()
-        viewIsHidden(isHidden: false)
+        tableView.isHidden = false
     }
 
     func show(error: String) {
