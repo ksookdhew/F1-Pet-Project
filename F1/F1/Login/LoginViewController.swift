a5add8c6a2afc33aff457fbcc245a8ea82df1462
Ksookdhew <89077850+Ksookdhew@users.noreply.github.com>
2024-05-26 11:42:48 +0200

    Keyboard adjustment

--- F1/F1/Login/LoginViewController.swift
+++ F1/F1/Login/LoginViewController.swift
@@ -21,8 +21,26 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         errorMessage.isHidden = true
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification, object: nil)
+
     }
 
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= 150
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
     // MARK: IBAction
     @IBAction func loginAction(_ sender: Any) {
         if viewModel.validDetails(givenUsername: username.text, givenPassword: password.text) {
