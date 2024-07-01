6e7dccbc7cfd284df7a10173a38b4552e28d595f
Ksookdhew <89077850+Ksookdhew@users.noreply.github.com>
2024-07-01 11:44:32 +0200

    Nav bar update

--- F1/F1/Config/AppDelegate.swift
+++ F1/F1/Config/AppDelegate.swift
@@ -14,15 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
          UINavigationBar.appearance().tintColor = .red
         if #available(iOS 15, *) {
-            let navigationBarAppearance = UINavigationBarAppearance()
-            navigationBarAppearance.configureWithOpaqueBackground()
-            navigationBarAppearance.titleTextAttributes = [
-                NSAttributedString.Key.foregroundColor: UIColor.white
-            ]
-            navigationBarAppearance.backgroundColor = .clear
-            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
-            UINavigationBar.appearance().compactAppearance = navigationBarAppearance
-            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+            UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+            UINavigationBar.appearance().shadowImage = UIImage()
 
             let tabBarAppearance = UITabBarAppearance()
             tabBarAppearance.configureWithOpaqueBackground()
