f5fadf5d0b6237475be7406c2cf3e5dae80d19fa
Ksookdhew <89077850+Ksookdhew@users.noreply.github.com>
2024-07-01 15:32:58 +0200

    code clean up

--- F1/F1/Utilities/DateFormatter.swift
+++ F1/F1/Utilities/DateFormatter.swift
@@ -13,8 +13,8 @@ extension DateFormatter {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy'-'MM'-'dd'"
         let date = dateFormatter.date(from: date) ?? Date()
-        let dateComps = Calendar.current.dateComponents([.year, .month, .day], from: date)
-        return dateComps
+        return Calendar.current.dateComponents([.year, .month, .day], from: date)
+
     }
 
     func customLocalTimeFormatter(time: String) -> String {
@@ -28,8 +28,6 @@ extension DateFormatter {
         let localDateFormatter = DateFormatter()
         localDateFormatter.dateFormat = "HH:mm:ss"
         localDateFormatter.timeZone = TimeZone.current
-        let localTimeString = localDateFormatter.string(from: formattedTime)
-
-        return localTimeString
+        return localDateFormatter.string(from: formattedTime)
     }
 }
